badd32dcc6bae69d3807f58793e2027e45e0a422
dbarrow257 <danbarrow257@gmail.com>
2023-02-17 12:57:14 +0000

    Full run through which can build mtuples without problem

--- SKSamples/RunScript_T2KReweight_MAQEh.sh
+++ SKSamples/RunScript_T2KReweight_MAQEh.sh
@@ -3,6 +3,6 @@ DIR=$PWD
 cd ../../MAQEhTools
 source ./setup.sh
 
-./OAGenWeightsApps/build/Linux/bin/genWeights_T2KSKAtm_MAQE_H -s ../FullMC/sk4_fcmc_tau_pcmc_ummc_fQv4r0_sf_minituple_500yr.root -o ../FullMC/sk4_fcmc_tau_pcmc_ummc_fQv4r0_sf_minituple_500yr_MAQE_Weights.root
+./OAGenWeightsApps/build/Linux/bin/genWeights_T2KSKAtm_MAQE_H -s ../SKSamples/FullMC/sk4_fcmc_tau_pcmc_ummc_fQv4r0_sf_minituple_500yr.root -o ../SKSamples/FullMC/sk4_fcmc_tau_pcmc_ummc_fQv4r0_sf_minituple_500yr_MAQE_Weights.root
 
 cd $DIR
